441a0631b0002a62a7bf9b0715f7cc9d541852ff
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-07 11:42:54 +0100

    refactor: rename call details model to CallLog

--- logger/models.py
+++ logger/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class CallDetails(models.Model):
+class CallLog(models.Model):
     extn = models.IntegerField()
     name = models.CharField(max_length=100)
     budget = models.IntegerField()
