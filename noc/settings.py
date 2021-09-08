eb925476ac8ed3df80832cd0757f1d7eefaa31e4
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-08 08:51:02 +0100

    refactor: extract common HTML to base template

--- noc/settings.py
+++ noc/settings.py
@@ -52,10 +52,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "noc.urls"
 
+import os
+
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(SETTINGS_PATH, "noc/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
