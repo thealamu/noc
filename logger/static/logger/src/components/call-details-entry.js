22fa63fac8276f2a666d016c8cf9a36170b1faa3
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-08 10:44:27 +0100

    feat: clear inputs after save

--- logger/static/logger/src/components/call-details-entry.js
+++ logger/static/logger/src/components/call-details-entry.js
@@ -118,6 +118,7 @@ export default {
                 remark: this.remark,
             }
             console.log(data)
+            this.clear()
         },
         onDestTypeChange(val) {
             this.isInternational = val.toLowerCase() === "international"
