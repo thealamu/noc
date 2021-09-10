545cd80da4d458dfdc98f061bc9e1c67dfb79702
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-10 16:29:54 +0100

    feat: sort the history items in descending order

--- history/views.py
+++ history/views.py
@@ -3,5 +3,5 @@ from logger.models import CallLog
 
 # Create your views here.
 def index(request):
-    logs = CallLog.objects.all()
+    logs = CallLog.objects.all().order_by("-id")
     return render(request, "history/index.html", {"logs": logs})
