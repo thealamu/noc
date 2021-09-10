73117ef204623ecbc19e27871d22b4b0271487e7
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-10 14:01:38 +0100

    fix: reverse admin path instead of hardcoding
    Nav links are underlined on hover

--- noc/urls.py
+++ noc/urls.py
@@ -18,8 +18,12 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
     path("", include("logger.urls")),
 ]
