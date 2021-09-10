55440c6efa30fa540da5dfd61e2990deade4df93
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-10 09:12:42 +0100

    feat: implement user logout

--- noc/urls.py
+++ noc/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("admin/", admin.site.urls),
     path("", include("logger.urls")),
 ]
