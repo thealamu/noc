b548f5ae03111c6ebfa40b2350ccf6a2fe2d9256
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-24 11:47:03 +0100

    feat: retrieve logs since range

--- reportgen/views.py
+++ reportgen/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from logger.models import CallLog
+from datetime import datetime, timedelta
 
 # Create your views here.
 def index(request):
-    return render(request, "reportgen/index.html")
+    report_range = request.GET.get("range")
+
+    if report_range.trim() == "":
+        report_range = "weekly"
+
+    report_range = report_range.lower()
+    since = None
+    if report_range == "weekly":
+        # get time of last sunday 12am
+        today = datetime.today()
+        since = today - timedelta(days=7)
+    elif report_range == "monthly":
+        # get the timestamp of the first day of the month
+        today = datetime.today()
+        since = datetime(today.year, today.month, 1)
+
+    # get all call logs since 'since'
+    call_logs = CallLog.objects.filter(created_at__gte=since)
