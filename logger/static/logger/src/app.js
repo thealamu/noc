991daa5b612cc2892db9c31623967d93db8ea3fa
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-27 08:42:01 +0100

    feat: center call logger

--- logger/static/logger/src/app.js
+++ logger/static/logger/src/app.js
@@ -5,8 +5,8 @@ export default {
         "call-details-entry": callDetailsEntry,
     },
     template: `
-        <div class="flex flex-row justify-end mx-4 my-4">
-            <div class="md:w-7/12">
+        <div class="flex flex-row justify-center mx-4 my-4">
+            <div class="">
                 <call-details-entry></call-details-entry>
             </div>
         </div>
